7a5588c032142c3da98886cb39f2343128207340
Fredrik Vedvik <fredrik@vedvik.tech>
2021-03-19 09:01:36 +0100

    gma

--- src/events/guildMemberAdd.ts
+++ src/events/guildMemberAdd.ts
@@ -1,9 +1,12 @@
 import { GuildMember } from 'discord.js';
+import { guilds } from '..';
 import sanity from '../classes/sanity';
 import config from '../config';
 
 export default async (member: GuildMember): Promise<void> => {
     if (config.debug) return console.log('DEBUG: TRUE | MEMBER JOIN IGNORED');
+
+    if (!guilds[member.guild.id].config.verifyMembers) return console.log("CHECKING NOT ENABLED ON SERVER");
 
     sanity.VerifyUser(member);
 };
